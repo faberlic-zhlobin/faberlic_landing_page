2ed512e62f046d11a6ce2a8d2ef4f23829c70f71
faberlic-zhlobin <platon02071963@gmail.com>
2025-06-24 18:16:41 +0300

    v8

--- js/timer.js
+++ js/timer.js
@@ -285,7 +285,7 @@ document.head.appendChild(style);
 
 document.addEventListener('DOMContentLoaded', () => {
     const currentYear = new Date().getFullYear();
-    const endDateString = `${currentYear}-06-22T23:59:59`; 
+    const endDateString = `${currentYear}-07-13T23:59:59`; 
     
     const timer = new CountdownTimer(endDateString);
     
